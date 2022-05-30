d92346c485770250e343e9e0d45849dfc0fedb36
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-05-30 07:58:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $files = scandir($currentFolder);
 echo '<ul>';
 
 foreach($files as $file) {
-   if (! is_dir($currentFolder . '/' . $file)) {
+   if (is_dir($currentFolder . '/' . $file)) {
       echo '<li>' . $file . '</li>';
    }
 }

3acaccb8f3bfa4ade648affb4fd4fa3a458da22d
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2022-05-30 07:57:50 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$currentFolder = './';
+$files = scandir($currentFolder);
+
+echo '<ul>';
+
+foreach($files as $file) {
+   if (! is_dir($currentFolder . '/' . $file)) {
+      echo '<li>' . $file . '</li>';
+   }
+}
+
+echo '</ul>';
